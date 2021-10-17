c1d409256c1e19c006b981cdb4b9f4118f089f0b
Jacob-Pilon <72111038+Jacob-Pilon@users.noreply.github.com>
2021-10-16 21:26:42 -0400

    Created Lab5 project
    simply created project, will finish tmrw

---
+++ app/src/main/java/gr49/uottawa/lab5_jacobpilon/MainActivity.java
@@ -0,0 +1,14 @@
+package gr49.uottawa.lab5_jacobpilon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/gr49/uottawa/lab5_jacobpilon/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package gr49.uottawa.lab5_jacobpilon;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
